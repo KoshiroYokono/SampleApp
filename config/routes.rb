8a1e0dd0839cac3e313eadb4c0d37f51de0e0bf0
横野公至郎 <yokonokoushirou@katsuo.local>
2018-04-28 17:21:36 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'favorites/index'
+
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  get '/login',   to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
+  delete 'logout',to: 'sessions#destroy'
+
+  resources 'users'
+  resources 'topics'
+  post '/favorites', to: 'favorites#create'
+  get '/favorites', to: 'favorites#index'
+  get '/comments/:id/index', to: 'comments#index'
+  post '/comments/create', to: 'comments#create', as: "comments"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
